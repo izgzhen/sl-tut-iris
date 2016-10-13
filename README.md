d8ad090ad836febb1276f581b5d47e5dfe329db9
Zhen Zhang <izgzhen@gmail.com>
2016-10-14 00:19:54 +0200

    Add basic info

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sl-tut-iris
+
+
+Source files for *Interactive Separation Logic Tutorial with Coq and Iris* (draft).
+
+
---
+++ src/Intro.v
@@ -0,0 +1,11 @@
+(** Introduction *)
+
+(** * Separation Logic *)
+
+(**
+Separation logic is an extension of Hoare logic. We can use it to reason about program with pointer --
+or more generally, one type of value that serves as a reference to another value in memory.
+
+Separation logic is developed by John C. Reynolds et al. at the start of this century. It is still being
+studied, extended, and revised actively today.
+*)
