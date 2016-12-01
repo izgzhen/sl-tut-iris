5277d0636d43c9a30cfeff025efab3c2f3d1e72d
Zhen Zhang <izgzhen@gmail.com>
2016-12-02 00:08:59 +0800

    More Tree

--- src/Tree.v
+++ src/Tree.v
@@ -1,6 +1,12 @@
 (** Tree *)
 
-(* Credit: Iris library *)
+(* Credit: Iris library.
+
+This is a continued example of recursive data structure in Iris, following the List.
+It also gives a more "useful" property and a function to specify with.
+
+However, note that this is still not related to concurrency yet.
+*)
 
 From iris.program_logic Require Export weakestpre.
 From iris.heap_lang Require Export lang.
@@ -46,34 +52,40 @@ Definition sum' : val := λ: "t",
 
 Global Opaque sum_loop sum'.
 
-Lemma sum_loop_wp `{!heapG Σ} v t l (n : Z) (Φ : val → iProp Σ) :
-  heap_ctx ∗ l ↦ #n ∗ is_tree v t
+Section proof.
+  Context `{!heapG Σ}.
+  Lemma sum_loop_wp v t l (n : Z) (Φ : val → iProp Σ) :
+    heap_ctx ∗ l ↦ #n ∗ is_tree v t
     ∗ (l ↦ #(sum t + n) -∗ is_tree v t -∗ Φ #())
-  ⊢ WP sum_loop v #l {{ Φ }}.
-Proof.
-  iIntros "(#Hh & Hl & Ht & HΦ)".
-  iLöb as "IH" forall (v t l n Φ). wp_rec. wp_let.
-  destruct t as [n'|tl tr]; simpl in *.
-  - iDestruct "Ht" as "%"; subst.
-    wp_match. wp_load. wp_op. wp_store.
-    by iApply ("HΦ" with "Hl").
-  - iDestruct "Ht" as (ll lr vl vr) "(% & Hll & Htl & Hlr & Htr)"; subst.
-    wp_match. wp_proj. wp_load.
-    wp_apply ("IH" with "Hl Htl"). iIntros "Hl Htl".
-    wp_seq. wp_proj. wp_load.
-    wp_apply ("IH" with "Hl Htr"). iIntros "Hl Htr".
-    iApply ("HΦ" with "[Hl]").
-    { by replace (sum tl + sum tr + n) with (sum tr + (sum tl + n)) by omega. }
-    iExists ll, lr, vl, vr. by iFrame.
-Qed.
+    ⊢ WP sum_loop v #l {{ Φ }}.
+  Proof.
+    iIntros "(#Hh & Hl & Ht & HΦ)".
+    iLöb as "IH" forall (v t l n Φ). wp_rec. wp_let.
+    (* Löb induction to solve recursion. Note how forall selects
+       variables to generalize. *)
+    destruct t as [n'|tl tr]; simpl in *.
+    (* we consider two cases: t is the leaf or t is the branch *)
+    - iDestruct "Ht" as "%"; subst.
+      wp_match. wp_load. wp_op. wp_store.
+      by iApply ("HΦ" with "Hl").
+    - iDestruct "Ht" as (ll lr vl vr) "(% & Hll & Htl & Hlr & Htr)"; subst.
+      wp_match. wp_proj. wp_load.
+      wp_apply ("IH" with "Hl Htl"). iIntros "Hl Htl".
+      wp_seq. wp_proj. wp_load.
+      wp_apply ("IH" with "Hl Htr"). iIntros "Hl Htr".
+      iApply ("HΦ" with "[Hl]").
+      { by replace (sum tl + sum tr + n) with (sum tr + (sum tl + n)) by omega. }
+      iExists ll, lr, vl, vr. by iFrame.
+  Qed.
 
-Lemma sum_wp `{!heapG Σ} v t Φ :
-  heap_ctx ∗ is_tree v t ∗ (is_tree v t -∗ Φ #(sum t))
-  ⊢ WP sum' v {{ Φ }}.
-Proof.
-  iIntros "(#Hh & Ht & HΦ)". rewrite /sum' /=.
-  wp_let. wp_alloc l as "Hl". wp_let.
-  wp_apply (sum_loop_wp with "[- $Hh $Ht $Hl]").
-  rewrite Z.add_0_r.
-  iIntros "Hl Ht". wp_seq. wp_load. by iApply "HΦ".
-Qed.
+  Lemma sum_wp v t Φ :
+    heap_ctx ∗ is_tree v t ∗ (is_tree v t -∗ Φ #(sum t))
+    ⊢ WP sum' v {{ Φ }}.
+  Proof.
+    iIntros "(#Hh & Ht & HΦ)". rewrite /sum' /=.
+    wp_let. wp_alloc l as "Hl". wp_let.
+    wp_apply (sum_loop_wp with "[- $Hh $Ht $Hl]").
+    rewrite Z.add_0_r.
+    iIntros "Hl Ht". wp_seq. wp_load. by iApply "HΦ".
+  Qed.
+End proof.
