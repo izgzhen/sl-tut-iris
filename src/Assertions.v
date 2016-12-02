a99292d73f65fe857e3e48b6ebb085826bf0466f
Zhen Zhang <izgzhen@gmail.com>
2016-12-02 14:04:22 +0800

    Fix format

--- src/Assertions.v
+++ src/Assertions.v
@@ -1,10 +1,9 @@
-(** Assertions *)
+(** * Basics *)
+
 From iris.program_logic Require Export weakestpre.
 From iris.heap_lang Require Export lang.
 From iris.proofmode Require Import tactics.
 From iris.heap_lang Require Import proofmode notation.
-
-(** * Basics *)
 
 Section basics.
   Context `{!heapG Σ}.
@@ -13,19 +12,20 @@ Section basics.
 
   Variable x: loc.
 
-  Definition foo : iProp Σ := (x ↦ #1)%I. (* %I is Iris scope, which types this expression as iProp Σ; # is used to transfrom literal to a general value *)
+  Definition foo : iProp Σ := (x ↦ #1)%I.
+  (* [%I] is Iris scope, which types this expression as [iProp Σ]; [#] is used to transfrom literal to a general value *)
 
-  (** A simple example: Assumption foo, which defines a singleton heap from x to 1,
-      entails that there is a singleton heap in which x points to integer literal *)
+  (** A simple example: [foo] defines a singleton heap from [x] to [1],
+      which means that there is a singleton heap in which [x] points to integer literal *)
   Example dummy: foo ⊢ ∃ n: Z, x ↦ #n.
   Proof.
     iIntros "Hfoo".
     by iExists 1.
   Qed.
 
-  (** Note that the "entails" and "exists" are lifted from the pure logic to the Iris logic *)
+  (** Note that the "entails" and "exists" are lifted from the pure logic to the Iris logic. *)
 
-  (** A more interesting lemma about commutivity of heap point-to *)
+  (** A more interesting lemma about commutivity of heap point-to: *)
 
   Lemma comm_heap: ∀ (y: loc) (xv yv: val),
       x ↦ xv ∗ y ↦ yv ⊢ y ↦ yv ∗ x ↦ xv.
@@ -34,16 +34,19 @@ Section basics.
     iSplitL "Hy"; done.
   Qed.
 
-  (** About ProofMode.
+  (** * About ProofMode:
 
 As you may find by now, there are many "magical" things in proving things in Coq with Iris:
 
 - Two Contexts: Pure (Coq) context and Spatial (Iris) context
-- Specialized tactics: e.g. iExists, iIntros, iSplitL. You may infer their menaing by comparing them with the pure versions: exists, intros, and split.
+- Specialized tactics: e.g. [iExists], [iIntros], [iSplitL]. You may infer their menaing by comparing them with the pure versions: [exists], [intros], and [split].
 - Intensive notations: These notations makes Coq scripts more readable, but sometimes, (very rarely thanks to Robbert) important things about types are
   hidden under these notations thus confusing newcomers a lot. You can use Set Printing Notations to observe this.
 
- In fact, this magical thing is called ProofMode, and it greatly enhances the experience of proving stuff in Iris both in terms of autamation, readability, spatial context management etc. As said in the paper[?], it makes "reasoning in the embedded logic as seamless as reasoning in the meta logic of the proof assistant".
+In fact, this magical thing is called ProofMode, and it greatly enhances the experience of proving stuff in Iris
+both in terms of autamation, readability, spatial context management etc.
+As said in the paper _Interactive Proofs in Higher-Order Concurrent Separation Logic_,
+it makes "reasoning in the embedded logic as seamless as reasoning in the meta logic of the proof assistant".
    *)
 
   (** So, where is an "empty heap"? *)
@@ -95,7 +98,7 @@ As you may find by now, there are many "magical" things in proving things in Coq
   Example mix: ∀ y: loc, (x = y) ∗ y ↦ #1 ⊢ x ↦ #1.
   Proof.
     iIntros (y) "[% Hy]". (* Use % to introduce a (unfortunately anonymous) pure assertion *)
-    by subst. (* Note: by [tactic] is equal to [tactic; done.] *)
+    by subst. (* Note: [by <tactic>] is equal to [<tactic>; done.] *)
   Qed.
 
   (** Finally, let's prove those "derived rules" in 2.4 *)
--- src/Intro.v
+++ src/Intro.v
@@ -1,5 +1,3 @@
-(** * Introduction *)
-
 (** * Separation Logic *)
 
 (**
